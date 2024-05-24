f4ecc6adc43cffd16ea9572395e4a0763ca5bd57
rosemondasilga <106432950+rosemondasilga@users.noreply.github.com>
2024-05-24 21:00:51 +0200

    all tests passed

--- React_component/task_0/dashboard/src/App/App.js
+++ React_component/task_0/dashboard/src/App/App.js
@@ -1,30 +1,50 @@
-import React from "react";
-import "./App.css";
-import logo from "../assets/logo.png";
-import { getFullYear, getFooterCopy } from "../utils/utils";
+import './App.css';
+import React from 'react';
+import { hot } from 'react-hot-loader';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <>
-      <div className="App-header">
-        <img src={logo} alt="logo"/>
-        <h1>School dashboard</h1>
-      </div>
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
-        <button>OK</button>
-      </div>
-      <div className="App-footer">
-        <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
-        </p>
-      </div>
-    </>
-  );
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
+import { getLatestNotification } from '../utils/utils';
+
+class App extends React.Component {
+  render() {
+    const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+    const htmlObj = getLatestNotification();
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New course available' },
+      { id: 3, type: 'urgent', html: htmlObj },
+    ]
+
+    return (
+      <>
+        <Notifications displayDrawer={ false } listNotifications={ listNotifications } />
+        <div className="App">
+          <Header />
+          { isLoggedIn ? <CourseList listCourses={ listCourses } /> : <Login /> }
+          <Footer />
+        </div>
+      </>
+    )
+  }
 }
 
-export default App;
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default hot(module)(App);
